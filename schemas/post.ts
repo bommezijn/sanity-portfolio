84697738618c4fb94b6241d193195b8dba548b2e
Nathan B <nathanbommezijn@gmail.com>
2024-02-08 15:12:52 +0100

    fix: allow body to have images

--- schemas/post.ts
+++ schemas/post.ts
@@ -48,22 +48,23 @@ export default defineType({
     defineField({
       name: 'body',
       title: 'Body',
-      type: 'array',
-      of: [
-        {
-          type: 'block',
-          styles: [
-            {title: 'Normal', value: 'normal'},
-            {title: 'H1', value: 'h1'},
-            {title: 'H2', value: 'h2'},
-            {title: 'H3', value: 'h3'},
-            {title: 'H4', value: 'h4'},
-            {title: 'H5', value: 'h5'},
-            {title: 'H6', value: 'h6'},
-            {title: 'Quote', value: 'blockquote'}
-          ]
-        }
-      ]
+      type: 'blockContent',
+      // type: 'array',
+      // of: [
+      //   {
+      //     type: 'block',
+      //     styles: [
+      //       {title: 'Normal', value: 'normal'},
+      //       {title: 'H1', value: 'h1'},
+      //       {title: 'H2', value: 'h2'},
+      //       {title: 'H3', value: 'h3'},
+      //       {title: 'H4', value: 'h4'},
+      //       {title: 'H5', value: 'h5'},
+      //       {title: 'H6', value: 'h6'},
+      //       {title: 'Quote', value: 'blockquote'}
+      //     ]
+      //   }
+      // ]
     }),
     defineField({
       type: 'code',
